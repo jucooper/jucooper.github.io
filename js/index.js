8c5608f7d86d4e55af3bc96297c0d6cc255cb53f
Justin Cooper <justinwcooper@outlook.com>
2018-05-13 14:55:17 -0400

    fade in text on scroll

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
-  $(".fade").hide().fadeIn(2000);
-  }
-);
+    $('.intro').hide().fadeIn(2000);
+    $('.palm-tree-icon').hide().fadeIn(2000);
+    $(window).scroll( function(){
+        $('.fade').each( function(i){
+            var bottom_of_object = $(this).position().top + ( $(this).outerHeight() - 500 );
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+              $(this).animate({'opacity':'1'}, 1500);
+            }
+        });
+    });
+});
