174fddf4e8080fc5279dfdabec261cbd30af3848
Justin Cooper <justinwcooper@outlook.com>
2018-05-16 21:26:29 -0400

    adds more styles

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
             var bottom_of_object = $(this).position().top + ( $(this).outerHeight() - 500 );
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             if( bottom_of_window > bottom_of_object ){
-              $(this).animate({'opacity':'1'}, 1500);
+              $(this).animate({'opacity':'1'}, 3000);
             }
         });
     });
