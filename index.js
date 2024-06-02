cff3dbc87104d381d2ac76feabec18b6f4ed8630
Justin Cooper <justinwcooper@outlook.com>
2024-06-02 09:32:09 -0400

    add goals and articles page

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
+// script.js
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.getElementById('toggle-button');
-    const icon = toggleButton.querySelector('i');
 
     toggleButton.addEventListener('click', () => {
         document.body.classList.toggle('light-mode');
-        if (document.body.classList.contains('light-mode')) {
-            icon.classList.remove('fa-moon');
-            icon.classList.add('fa-sun');
-        } else {
-            icon.classList.remove('fa-sun');
-            icon.classList.add('fa-moon');
-        }
     });
 });
